e9dcb20819d9d4854d2fe1a18561022f6e0727f2
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-09-29 00:37:24 +0530

    reversing array without .reverse()

--- JS-BASIC-Programs/reverseArray.js
+++ JS-BASIC-Programs/reverseArray.js
@@ -1,3 +1,13 @@
-function reverseArray() {
-  const arr = arr.reverse();
+// Executing array reverse without using reverse function 
+
+function arrayReverse(arr){
+  let reversedArrray = []
+
+  for(let i = arr.length - 1; i >= 0; i--){
+    reversedArrray.push(arr[i])
+  }
+
+  return reversedArrray;
 }
+
+console.log(arrayReverse([1,2,3,4,5]));
